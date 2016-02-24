6c8449191277b33ad3120f8abb51a14e94d7327a
Darren Foong <darrenfoong@gmail.com>
2016-02-24 15:17:34 +0000

    Add distance and POS tags

--- consolidatedeps.py
+++ consolidatedeps.py
@@ -62,17 +62,22 @@ def unconvert_lines(f):
         f = f.replace(current, repl)
     return f
 
-def canonize(dep):
+def canonize(dep, pos_tags):
     # need to strip indices and possibly add additional dep info
     dep_values = dep.split(" ")
-    dep_values[0] = "_".join(dep_values[0].split("_")[:-1])
-    dep_values[3] = "_".join(dep_values[3].split("_")[:-1])
+    head = dep_values[0].split("_")
+    dependent = dep_values[3].split("_")
+    dep_values[0] = "_".join(head[:-1])
+    dep_values[3] = "_".join(dependent[:-1])
+    dep_values[4] = abs(dependent - head)
+    dep_values[5] = pos_tags[head]
+    dep_values[6] = pos_tags[dependent]
     dep = " ".join(dep_values)
     return dep
 
-def add(dep, inc):
+def add(dep, inc, pos_tags):
     if dep != "":
-        dep = canonize(dep)
+        dep = canonize(dep, pos_tags)
         if dep in deps:
             deps[dep] += inc
         else:
@@ -80,12 +85,17 @@ def add(dep, inc):
 
 with open(WORKING_DIR + "deps_correct", "w") as output_correct_deps_file, \
      open(WORKING_DIR + "deps_incorrect", "w") as output_incorrect_deps_file, \
-     open("../data/gold/wsj02-21.ccgbank_deps", "r") as correct_deps_file:
+     open("../data/gold/wsj02-21.ccgbank_deps", "r") as correct_deps_file, \
+     open("../data/gold/wsj02-21.stagged.reformat", "r") as gold_supertags_file:
 
     while correct_deps_file.readline().startswith("#"):
         pass
 
+    while gold_supertags_file.readline().startswith("#"):
+        pass
+
     correct_deps_sents = convert_lines(correct_deps_file.read()).split("\n\n")[:-1]
+    gold_supertags_sents = convert_lines(gold_supertags_file.read()).split("\n\n")[:-1]
 
     print("Number of sentences in correct_deps: " + str(len(correct_deps_sents)))
 
@@ -104,12 +114,15 @@ with open(WORKING_DIR + "deps_correct", "w") as output_correct_deps_file, \
             for incorrect_dep_sent in incorrect_deps_sents:
                 incorrect_deps = incorrect_dep_sent.split("\n")
                 correct_deps = correct_deps_sents[correct_deps_sent_ptr].split("\n")
+                gold_supertags = gold_supertags_sents[correct_deps_sent_ptr].split("\n")
+
+                pos_tags = map((lambda e: e.split(" ")[1]), gold_supertags)
 
                 for incorrect_dep in set(incorrect_deps):
-                    add(incorrect_dep, -1)
+                    add(incorrect_dep, -1, pos_tags)
 
                 for correct_dep in set(correct_deps):
-                    add(correct_dep, 1)
+                    add(correct_dep, 1, pos_tags)
 
                 correct_deps_sent_ptr += 1
 
