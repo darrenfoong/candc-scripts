e9589b72b292eedf120487720e2f87ab28107898
Darren Foong <darrenfoong@gmail.com>
2016-04-07 20:15:26 +0100

    Update extract_roots.pl

--- extract_roots.pl
+++ extract_roots.pl
@@ -1,13 +1,24 @@
 #!/usr/bin/perl
 
-$c = 1;
-while(<>){
-    if(/^\#\#\#/){
-	$line = <>;
-	$line =~ /^\S+ \*\*\* (\S+) /;
-	print "$1\n";
 
-	print "$c EMPTY!\n" if ($1 eq "");
-	$c++;
+$pipe_file = $ARGV[0];
+open(PIPE, $pipe_file) || die("can't open pipe file\n");
+
+print "# obtained by running extract_roots.pl on $pipe_file\n\n";
+
+$c = 1;
+while(<PIPE>){
+    if(/^\#\#\#/){
+        $line = <PIPE>;
+        $line =~ /^\S+ \*\*\* (\S+) /;
+        die "$c EMPTY!\n" if ($1 eq "");
+
+        $cat = $1;
+        if($cat =~ /[\/\\]/){
+            $cat = "($cat)";
+        }
+        print "$cat\n";
+
+        $c++;
     }
 }
