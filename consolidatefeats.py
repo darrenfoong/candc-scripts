73d376729ff51c462ff57d59ae3a9a7774f7c1e9
Darren Foong <darrenfoong@gmail.com>
2016-06-14 21:08:00 +0100

    Modify consolidatefeats.py

--- consolidatefeats.py
+++ consolidatefeats.py
@@ -2,36 +2,45 @@
 
 # The following file structure is assumed:
 # output
-# |-incorrect_deps_test
+# |-incorrect_feats
 #   > oracle.out.feats
-#  |-split1
-#    > parser.beam.out.chartfeats
-# |-incorrect_deps
+#   > parser.beam.out.chartfeats
+# |-incorrect_feats_dev
 #   > oracle.out.feats
-#  |-split1
-#  ...
-#  |-splitN
-#    > parser.beam.out.chartfeats
+#   > parser.beam.out.chartfeats
+# |-incorrect_feats_test
+#   > oracle.out.feats
+#   > parser.beam.out.chartfeats
 # scripts
 # > consolidatefeats.py (this script)
 
 # The following files will be created:
 # output
-# |-incorrect_deps_test
+# |-incorrect_feats
 #   > feats_correct
 #   > feats_incorrect
-# |-incorrect_deps
+# |-incorrect_feats_dev
+#   > feats_correct
+#   > feats_incorrect
+# |-incorrect_feats_test
 #   > feats_correct
 #   > feats_incorrect
 
 import sys
 import os
 import re
+import itertools
 
-WORKING_DIR = "../output/incorrect_deps/"
-CORRECT_FEATS_FILE = "../output/incorrect_deps/oracle.out.feats"
+WORKING_DIR = "../output/incorrect_feats/"
 
-NUM_CHUNKS = 10
+if len(sys.argv) > 1:
+    if sys.argv[1] == "dev":
+        WORKING_DIR = "../output/incorrect_feats_dev/"
+    elif sys.argv[1] == "test":
+        WORKING_DIR = "../output/incorrect_feats_test/"
+
+CORRECT_FEATS_FILE = WORKING_DIR + "/oracle.out.feats"
+CHART_FEATS_FILE = WORKING_DIR + "/parser.beam.out.chartfeats"
 
 CONVERT_MAX = 10
 CONVERT_MIN = 3
@@ -43,14 +52,6 @@ CORRECT_THRESHOLD = 0
 
 # classfied as incorrect if feat < INCORRECT_THRESHOLD
 INCORRECT_THRESHOLD = 1
-
-if len(sys.argv) > 1:
-    if sys.argv[1] == "test":
-        NUM_CHUNKS = 1
-        WORKING_DIR = "../output/incorrect_deps_test/"
-        CORRECT_FEATS_FILE = "../output/incorrect_deps_test/oracle.out.feats"
-    else:
-        NUM_CHUNKS = int(sys.argv[1])
 
 feats = dict()
 
@@ -80,47 +81,47 @@ def unconvert_lines(f):
 
 def add(feat, inc):
     if feat != "":
-        if feat in feats:
-            feats[feat] += inc
+        if feat not in feats:
+            feats[feat] = (0, 0)
+
+        pos, neg = feats[feat]
+        if inc == 1:
+            feats[feat] = (pos+1, neg)
         else:
-            feats[feat] = inc
+            feats[feat] = (pos, neg+1)
 
 with open(WORKING_DIR + "feats_correct", "w") as output_correct_feats_file, \
      open(WORKING_DIR + "feats_incorrect", "w") as output_incorrect_feats_file, \
-     open(CORRECT_FEATS_FILE, "r") as correct_feats_file:
+     open(CORRECT_FEATS_FILE, "r") as correct_feats_file, \
+     open(CHART_FEATS_FILE, "r") as chart_feats_file:
 
     while correct_feats_file.readline().startswith("#"):
         pass
 
+    while chart_feats_file.readline().startswith("#"):
+        pass
+
     correct_feats_sents = convert_lines(correct_feats_file.read()).split("\n\n")[:-1]
+    chart_feats_sents = convert_lines(chart_feats_file.read()).split("\n\n")[:-1]
 
     print("Number of sentences in correct_feats: " + str(len(correct_feats_sents)))
+    print("Number of sentences in chart_feats: " + str(len(chart_feats_sents)))
 
     correct_feats_sent_ptr = 0
 
-    for i in range(1, NUM_CHUNKS+1):
-        with open(WORKING_DIR + "split" + str(i) + "/oracle.out.chartfeats", "r") as chart_feats_file:
+    for chart_feat_sent in chart_feats_sents:
+        print("Processing sentence " + str(correct_feats_sent_ptr+1))
 
-            while chart_feats_file.readline().startswith("#"):
-                pass
+        chart_feats = chart_feat_sent.split("\n")
+        correct_feats = correct_feats_sents[correct_feats_sent_ptr].split("\n")[:-1]
 
-            chart_feats_sents = convert_lines(chart_feats_file.read()).split("\n\n")[:-1]
+        for incorrect_feat in set(chart_feats) - set(correct_feats):
+            add(incorrect_feat, -1)
 
-            print("Number of sentences in chart feats (split " + str(i) + "): " + str(len(chart_feats_sents)))
+        for correct_feat in set(correct_feats):
+            add(correct_feat, 1)
 
-            for chart_feat_sent in chart_feats_sents:
-                print("Processing sentence " + str(correct_feats_sent_ptr+1))
-                chart_feats = chart_feat_sent.split("\n")
-
-                correct_feats = correct_feats_sents[correct_feats_sent_ptr].split("\n")
-
-                for incorrect_feat in set(chart_feats) - set(correct_feats):
-                    add(incorrect_feat, -1)
-
-                for correct_feat in set(correct_feats):
-                    add(correct_feat, 1)
-
-                correct_feats_sent_ptr += 1
+        correct_feats_sent_ptr += 1
 
     output_correct_feats_file.write("#\n\n")
     output_incorrect_feats_file.write("#\n\n")
@@ -132,14 +133,15 @@ with open(WORKING_DIR + "feats_correct", "w") as output_correct_feats_file, \
     tie_count = 0
 
     for feat, value in sorted(feats.iteritems(), key=lambda x: x[1]):
-        if value > CORRECT_THRESHOLD:
-            correct_count += 1
-            output_correct_feats_file.write(feat + " 1 " + str(value) + "\n")
-        else if value < INCORRECT_THRESHOLD:
-            if value == 0:
-                tie_count += 1
-            incorrect_count += 1
-            output_incorrect_feats_file.write(feat + " 0 " + str(value) + "\n")
+        pos, neg = value
+
+        for _ in itertools.repeat(None, pos):
+            output_correct_feats_file.write(feat + " 1 " + str(pos) + "\n")
+        for _ in itertools.repeat(None, neg):
+            output_incorrect_feats_file.write(feat + " 0 " + str(neg) + "\n")
+
+        correct_count += pos
+        incorrect_count += neg
 
     print "Correct feats: " + str(correct_count)
     print "Incorrect feats: " + str(incorrect_count)
